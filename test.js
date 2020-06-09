5cf826e7175c97ed5e12d7f83fcf00134b87f664
Harsh Padhye <hp9rup@virginia.edu>
2020-06-09 16:07:50 -0400

    Removed unnecessary files

--- test.js
+++
@@ -1,9 +0,0 @@
-//simple hello world program
-
-var http = require("http");
-
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("alexa play the box by roddy rich");
-    response.end();
-}).listen(8888);
