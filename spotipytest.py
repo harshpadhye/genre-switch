c109891f1db613ca38a82885e149d83045b8bbbf
Harsh Padhye <hp9rup@virginia.edu>
2020-06-24 11:19:23 -0400

    removed non-spotipy code

--- spotipytest.py
+++ spotipytest.py
@@ -13,7 +13,7 @@ if token:
     sp = spotipy.Spotify(auth=token)
 
     # Gets the top ten tracks for the user in the last 4 weeks
-    results = sp.current_user_top_tracks(limit=10, time_range="short_term")
+    results = sp.current_user_top_tracks(limit=15, time_range="short_term")
     for i, item in enumerate(results["items"]):
         # Allows console to print all artists of a track
         artists = [art_obj["name"] for art_obj in item["artists"]]
@@ -21,10 +21,10 @@ if token:
 
     # Searches for a dedicated "My Recent Favorites" playlist
     user_playlists = sp.current_user_playlists(limit=50, offset=0)
-    
+
     pl_exists = False
     pl_id = None
-    
+
     # Iterates through playlist objects and finds the appropriate playlist id
     for pl in user_playlists["items"]:
         if pl["name"] == "My Recent Favorites":
@@ -34,10 +34,11 @@ if token:
     # Dedicated playlist does not exist; let's create it
     if not pl_exists:
         pl_id = sp.user_playlist_create(user=username, name="My Recent Favorites",
-         public=True, description="My top ten most played tracks in the last month")["id"]
+                                        public=True, description="My top ten most played tracks in the last month")["id"]
 
     # Now that we have the correct playlist ID, we can rewrite its contents
     track_ids = [item["id"] for item in results["items"]]
-    sp.user_playlist_replace_tracks(user=username, playlist_id=pl_id, tracks=track_ids)
+    sp.user_playlist_replace_tracks(
+        user=username, playlist_id=pl_id, tracks=track_ids)
 
     print("\nAll done! Check your Spotify playlist library.")
