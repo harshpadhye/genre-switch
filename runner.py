4bfc4e62d4c1d81a6da11e83b682349c5aed27c2
Harsh Padhye <hp9rup@virginia.edu>
2020-06-25 11:07:11 -0400

    Organized funcs into sep. class for customization

---
+++ runner.py
@@ -0,0 +1,27 @@
+import spotipy
+import spotipy.util as util
+from spotipy_client import SpotipyClient
+
+# Required scopes to access top tracks, modify playlists
+scope = "user-top-read playlist-modify-public playlist-modify-private "
+
+username = input("Provide a username / Spotify ID: ")
+
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    # Creates Spotipy client
+    client = SpotipyClient(auth_token=token)
+
+    # Gets the top ten tracks for the user in the last 4 weeks
+    top_tracks = client.get_top_tracks(num_tracks=20, time_period="short_term")
+
+    # Searches for a dedicated "My Recent Favorites" playlist
+    my_recent_fav_playlist_id = client.create_new_playlist(pl_user=username, pl_name="My Recent Favorites",
+                                                        pl_description="My most frequently replayed tracks", pl_public=True)
+
+    # Now that we have the correct playlist ID, we can rewrite its contents
+    top_track_ids = [track["id"] for track in top_tracks]
+    client.rewrite_playlist(pl_user=username, pl_id=my_recent_fav_playlist_id, track_ids=top_track_ids)
+
+    print("\nAll done! Check your Spotify playlist library.")
---
+++ spotipy_client.py
@@ -0,0 +1,56 @@
+# Wrapper class to consolidate Spotipy method and add some custom functionalities
+
+import spotipy
+import spotipy.util as util
+
+class SpotipyClient:
+
+    # initializes the client with and authorization token
+    def __init__(self, auth_token):
+        self.sp = spotipy.Spotify(auth=auth_token)
+
+
+    # retrieves the top tracks for the user based on 
+    # frequency of plays within a given time period
+    # short_term, medium_term, long_term
+    def get_top_tracks(self, num_tracks, time_period):
+
+        results = self.sp.current_user_top_tracks(limit=num_tracks, time_range=time_period)
+
+        # prints tracks and artists to console in order
+        for i, item in enumerate(results["items"]):
+            artists = [art_obj["name"] for art_obj in item["artists"]]
+            print("\n" + str(i+1) + ".", item["name"], "-", *artists)
+
+        # returns the list of Track Objects
+        return results["items"]
+    
+
+    # Searches the user's playlist directory for the desired playlist name.
+    # if the playlist doesn't exist, create it; then, return that playlist id
+    def create_new_playlist(self, pl_user, pl_name, pl_description, pl_public):
+        # first, search if the playlist name already exists
+        # Spotify allows for the creation of duplicate playlist names
+        user_playlists = self.sp.current_user_playlists(limit=50, offset=0)
+        exists = False
+        id = None
+
+        # Iterates through playlist objects and finds the appropriate playlist id
+        for pl in user_playlists["items"]:
+           if pl["name"] == pl_name:
+                id = pl["id"]
+                exists = True
+                break
+
+        # Dedicated playlist does not exist; let's create it
+        if not exists:
+            id = self.sp.user_playlist_create(user=pl_user, name=pl_name, description=pl_description, public=pl_public)["id"]
+
+        # return the new/dedicated playlist id
+        return id
+    
+
+    # Given a playlist id and a list of track ids,
+    # replaces current tracks in the playlist with the new tracks
+    def rewrite_playlist(self, pl_user, pl_id, track_ids):
+        self.sp.user_playlist_replace_tracks(user=pl_user, playlist_id=pl_id, tracks=track_ids)
